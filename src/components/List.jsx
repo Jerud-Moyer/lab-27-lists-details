c5d1b0412f944c7034097d43a92e204701e14903
Jerud-Moyer <jerudmoyer@gmail.com>
2020-10-20 14:54:54 -0700

    names rendering

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-
+import { getCharacters } from '../services/characters-api';
 export default class List extends Component {
+  state = { names: [] }
 
-
-
-
+  componentDidMount = async() => {
+    const data = await getCharacters();
+    
+    this.setState({
+      names: data
+    });
+  }
 
   render() {
+    const { names } = this.state;
+
+    const nameElements = names.map(name => (
+      <li key={name}>
+        <span>{name.name}</span>
+      </li>
+    ));
+
     return (
-      <h1>list</h1>
+      <ul>
+        {nameElements}
+      </ul>
     );
   }
 
